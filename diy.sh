dd84462905def12b54a25e95235ab072343d040d
z <haig8@msn.com>
2020-03-11 21:11:31 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -7,3 +7,57 @@
 #=================================================
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+
+gitcheckout() {
+  git --git-dir=$1/.git --work-tree=$1 checkout .
+}
+
+patchdir() {
+  for i in $1/*.patch; do
+    patch -Ntp$2 < $i
+  done
+}
+
+dir=`dirname $0`
+device=$1
+if [ "$device" = "" ]; then
+  device=acrh17
+fi
+
+if [ ! -f "$dir/$device.config" ]; then
+  echo device config file not found
+  exit 1
+fi
+
+git clone --depth 1 https://github.com/yhfudev/openwrt-fcgiwrap.git ../feeds/fcgiwrap
+pushd ../feeds/fcgiwrap
+  # gitcheckout .
+  patchdir ../../patches/fcgiwrap 4
+popd
+
+# 重置文件
+# gitcheckout .
+# gitcheckout feeds/luci
+
+# 更新
+# git pull
+
+# 打补丁
+patchdir ../patches 1
+
+# 更新源
+# ./scripts/feeds update -a
+# ./scripts/feeds install -a
+
+# 文件
+# ln -sfn ../../files/localmirrors scripts/
+# ln -sfn ../../../../../files/0001-fstools-fix-libblkid-tiny-ntfs-uuid-detection.patch package/system/fstools/patches/
+
+rm -f .config*
+touch .config
+
+cat $dir/common.config >> .config
+cat $dir/$device.config >> .config
+
+sed -i 's/^[ \t]*//g' .config
+make defconfig
